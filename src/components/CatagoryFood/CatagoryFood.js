dc53df947c640721f5679e56cc43c9c1e3b0f110
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-10-09 12:59:58 +0300

    menu options added

---
+++ src/components/CatagoryFood/CatagoryFood.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CatagoryFood = () => {
+    return (
+        <div className="bg-[url('https://images.unsplash.com/photo-1657299156538-e08595d224ca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')] bg-cover p-36 rounded-lg bg-opacity-20 relative">
+            <div className='inset-0 bg-gray-900 absolute bg-opacity-75 rounded-lg'></div>
+            <div className='relative'>
+               <p className='text-white font-bold text-3xl '>Food By Category</p>
+            </div>
+        </div>
+    );
+};
+
+export default CatagoryFood;
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import CatagoryFood from '../CatagoryFood/CatagoryFood';
+import CountryFood from '../CountryFood/CountryFood';
 
 const Menu = () => {
     return (
         <div>
-            <h1>This is menu</h1>
+            <div className='bg-logo-text-color py-8'>
+                <p className='text-3xl font-semibold '>Bon appetit!</p>
+            </div>
+            <div className='flex gap-5 flex-row justify-center  mt-10'>
+                <CountryFood></CountryFood>
+                <CatagoryFood></CatagoryFood>
+            </div>
+            
         </div>
     );
 };
