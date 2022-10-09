22fb981b9ca271aca4efabd7b4f2b51b0235f502
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-10-09 17:41:38 +0300

    country meals added

--- src/components/CountryFoods/CountryFoods.js
+++ src/components/CountryFoods/CountryFoods.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import CountryRecipies from '../CountryRecipies/CountryRecipies';
 
 const CountryFoods = () => {
    const data = useLoaderData();
-   console.log(data)
+   const meals = data.meals;
     return (
-        <div>
-            <p>countryfoods: {}</p>
+        <div className='px-10 lg:px-20 text-center '>
+            <p className='text-lg lg:text-2xl font-bold p-10'>Total Item Found: {meals.length}</p>
+           <div className='grid lg:grid-cols-3 grid-cols-1  gap-10 '>
+                {
+                    meals.map((meal, idx) => <CountryRecipies key={idx} meal={meal}></CountryRecipies>)
+                }
+           </div>
+            
         </div>
     );
 };
