461089d361b0cd5af110df8ebacbe9b3332b6d42
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-10-09 19:56:08 +0300

    catagory food data display

--- src/components/CatagoryFoods/CatagoryFoods.js
+++ src/components/CatagoryFoods/CatagoryFoods.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import CatagoryRecipies from '../CatagoryRecipies/CatagoryRecipies';
 
 const CatagoryFoods = () => {
     const data = useLoaderData();
     const meals = data.meals;
-    console.log(meals)
     return (
-        <div>
-            <p>catagoryfoods</p>
+        <div className='px-10 lg:px-20 text-center '>
+            <p className='text-lg lg:text-2xl font-bold p-10'>Total Item Found: {meals.length}</p>
+            <div className='grid lg:grid-cols-3 grid-cols-1  gap-10 '>
+                {
+                    meals.map((meal, idx) => <CatagoryRecipies key={idx} meal={meal}></CatagoryRecipies>)
+                }
+            </div>
         </div>
     );
 };
---
+++ src/components/CatagoryRecipies/CatagoryRecipies.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const CatagoryRecipies = ({meal}) => {
+    const {strMeal, strMealThumb} = meal;
+    return (
+            <div className="card bg-base-100 shadow-xl">
+                <figure><img src={strMealThumb} alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">{strMeal}</h2>
+                </div>
+            </div>
+    );
+};
+
+export default CatagoryRecipies;
